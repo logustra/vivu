cae6ad9d21ef885373aaf3e8aee104ffa0c81c1b
Faizal Andyka <logustra@outlook.com>
2021-03-10 15:19:28 +0700

    fix: husky

--- husky.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  hooks: {
-    'pre-commit': 'pnpm lint',
-    'prepare-commit-msg': 'exec < /dev/tty && git cz --hook || true',
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-    'pre-push': 'pnpm build'
-  }
-}
