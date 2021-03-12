c5097a9e191d1d2af8960d129f1947e4564c0bd4
logustra <logustra@gmail.com>
2021-03-12 17:03:58 +0700

    refactor(create-cli): template service

--- create/templates/modules/services/exampleService.ts
+++ create/templates/modules/services/exampleService.ts
@@ -3,13 +3,21 @@ import Http from '@/services/Http'
 export default class ExampleService {
   protected http: Http
 
-  public constructor (http: Http) {
+  /**
+   * @param  {Http} http
+   */
+  public constructor(http: Http) {
     this.http = http
   }
 
-  public async get (url: string, params?: {}, responseType?: string) {
+  /**
+   * @param  {string} url
+   * @param  {{}} params?
+   * @param  {{}} config?
+   */
+  public async get(url: string, params?: {}, config?: {}) {
     try {
-      return await this.http.get(url, params, responseType)
+      return await this.http.get(url, params, config)
     } catch (error) {
       throw error
     }
