5cd0d2ad34a347de91c479ddb940224696398a39
logustra <logustra@gmail.com>
2021-03-17 11:13:13 +0700

    refactor: template store

--- create/templates/modules/stores/exampleMutations.ts
+++ create/templates/modules/stores/exampleMutations.ts
@@ -7,14 +7,15 @@ import {
 const mutations: {[key: string]: Function} = {
   [types.EXAMPLE_REQUEST]: (state: ExampleState) => {
     state.isFetching = true
+    state.isError = false
   },
 
   [types.EXAMPLE_SUCCESS]: (state: ExampleState, response: ExampleDataModel[]) => {
     state.data = response
+    state.isFetching = false
   },
 
   [types.EXAMPLE_ERROR]: (state: ExampleState, response: boolean) => {
-    state.data = []
     state.isFetching = false
     state.isError = response
   }
