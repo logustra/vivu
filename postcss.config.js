ab250649f3f9f311ef22dca242d465438e32058e
logustra <logustra@gmail.com>
2021-05-20 21:11:11 +0700

    fix: tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('@tailwindcss/jit'),
     require('autoprefixer')
   ]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  mode: 'jit',
   purge: [
     './index.html',
     './src/**/*.{vue,js,ts}'
